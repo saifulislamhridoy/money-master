af31d0e5322c0535ae42fee1bc5228064e77b818
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-02-17 17:25:22 +0600

    update

---
+++ JS/app.js
@@ -0,0 +1,57 @@
+function getInputValue(className){
+    const inputField = document.getElementById(className);
+    const getValue = inputField.value 
+    inputField.value=''
+    return getValue;  
+}
+function getBalance(){
+     // income field
+     const income =getInputValue('monthly-income');
+     //    Expenses field
+     // food cost
+     const foodCost = getInputValue('food-cost');
+     // rent cost
+     const rentCost = getInputValue('rent-cost');
+     // clothes cost 
+     const clothesCost = getInputValue('clothes-cost');
+     // total expenses
+     const totalExpenses = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+         console.log(totalExpenses)
+     
+     // update total expenses
+        const expenses = document.getElementById('total-expenses');
+        expenses.innerText =totalExpenses;
+     // //    update blance
+     const updateBalance = parseFloat(income) - totalExpenses;
+      const balance = document.getElementById('balance');
+      balance.innerText=updateBalance  
+      return updateBalance 
+}
+document.getElementById('calc-btn').addEventListener('click', function(){
+    // income field
+  const income =getInputValue('monthly-income');
+//    Expenses field
+// food cost
+const foodCost = getInputValue('food-cost');
+// rent cost
+const rentCost = getInputValue('rent-cost');
+// clothes cost 
+const clothesCost = getInputValue('clothes-cost');
+// total expenses
+const totalExpenses = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+    console.log(totalExpenses)
+
+// update total expenses
+   const expenses = document.getElementById('total-expenses');
+   expenses.innerText =totalExpenses;
+// //    update blance
+const updateBalance = parseFloat(income) - totalExpenses;
+const balance = document.getElementById('balance');
+balance.innerText=updateBalance  
+return updateBalance 
+})
+document.getElementById('save-btn').addEventListener('click', function(){
+  const save=  getInputValue('save')
+  const balance = getBalance()
+  
+})
