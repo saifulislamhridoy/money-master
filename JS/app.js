518a26dd73ed3e27985da8a5a66fda5cfdccd929
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-02-17 19:31:36 +0600

    update remaining

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,14 @@
 function getInputValue(className){
     const inputField = document.getElementById(className);
     const getValue = inputField.value;
-    inputField.value='';
+    // inputField.value='';
     return getValue;  
 }
 document.getElementById('calc-btn').addEventListener('click', function(){
+    // error handle
+    // if(income=="" || foodCost=="" || rentCost=="" || clothesCost==""){
+    //     return
+    // }
     // income field
   const income =getInputValue('monthly-income');
 //    Expenses field
@@ -29,9 +33,12 @@ balance.innerText=updateBalance;
 })
 
 document.getElementById('save-btn').addEventListener('click', function(){
-    const save1 = getInputValue('save-input')
-    const mone23 = document.getElementById('balance').innerText
-    const result = parseFloat(mone23) * parseFloat(save1) /100
-    console.log(result)
+    const saveInput = getInputValue('save-input')
+    const income = getInputValue('monthly-income')
+    const totalSave = parseFloat(income) * parseFloat(saveInput) /100
+    document.getElementById('saving-amount').innerText =totalSave
+     const priviousBalance = document.getElementById('balance').innerText
+     const remainingBalance = parseFloat(priviousBalance) - totalSave
+     document.getElementById('remaining-balance').innerText=remainingBalance
 })
  
